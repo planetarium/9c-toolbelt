a1ef820296aa090f9f1245bd736dd61e29e28a13
Robert <35055316+sky1045@users.noreply.github.com>
2022-12-23 11:08:14 +0900

    test pip3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -ex
 
 echo $COMMAND
 
+pip3 freeze
+which python3
+
 if [[ $COMMAND = "prepare" ]]; then
   planet key import --passphrase $KEY_PASSPHRASE $KEY_PRIVATE
 
@@ -10,4 +13,6 @@ if [[ $COMMAND = "prepare" ]]; then
   python3 /root/cli.py prepare release $network $rc_number $deploy_number --launcher-commit "$launcher_commit" --player-commit "$player_commit" --slack-channel "$SLACK_CHANNEL"
 elif [[ $COMMAND = "update" ]]; then
   python3 /root/cli.py update release-infos
+else
+  python3 /root/cli.py --help
 fi
