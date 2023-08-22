1a252ee721ef4c61f115f6d6861eac14f0ffaff8
Jiwon <me@atralupus.com>
2023-08-22 11:48:01 +0900

    fix: Handle semver versioning (#32)

--- toolbelt/apps/release/__init__.py
+++ toolbelt/apps/release/__init__.py
@@ -10,35 +10,45 @@ from .update_latest_metadata import update as update_latest_metadata
 release_app = typer.Typer()
 
 
+def convert_version(version):
+    x, y, z = version.split(".")
+    return int(x.zfill(4) + y.zfill(4) + z.zfill(4))
+
+
 @release_app.command()
 def player(
     commit_hash: str,
-    version: int,
+    version: str,
     network: str = network_arg,
     platform: str = platforms_arg,
     signing: bool = False,
     slack_channel: Optional[str] = None,
     run_id: Optional[str] = None,
 ):
+    converted_version = convert_version(version)
+
     release_player(
         commit_hash,
         platform,  # type:ignore
-        version,
+        converted_version,
         network,  # type:ignore
         signing,
         slack_channel,
-        run_id
+        run_id,
     )
+
 
 @release_app.command()
 def update_latest(
     commit_hash: str,
-    version: int,
+    version: str,
     network: str,
     slack_channel: Optional[str] = None,
 ):
+    converted_version = convert_version(version)
+
     update_latest_metadata(
-        version,
+        converted_version,
         commit_hash,
         network,
         slack_channel,
